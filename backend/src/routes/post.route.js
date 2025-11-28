27f02a876d63b26db603dde53eefac0da8aebd45
Sathvik606 <sathvikch606@gmail.com>
2025-11-28 19:58:58 +0530

    feat: add CRUD APIs

---
+++ backend/src/routes/post.route.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { createPost, deletePost, getPosts, updatePost } from "../controllers/post.controller.js";
+
+
+
+const postRouter = Router();
+
+postRouter.route('/create').post(createPost)
+postRouter.route('/getPosts').get(getPosts)
+postRouter.route('/update/:id').patch(updatePost)
+postRouter.route('/delete/:id').delete(deletePost)
+
+export default postRouter;
